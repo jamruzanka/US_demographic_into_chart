3a612014934162033bd88379e4a11a99474e86f1
jamruzanka <m.w.jamrog@gmail.com>
2018-12-13 23:03:02 +0100

    changed script - added classes

--- US_demographic_small.py
+++ US_demographic_small.py
@@ -1,35 +1,47 @@
 from openpyxl import Workbook
 from openpyxl.chart import Reference, Series, BarChart
 
-my_excel = Workbook()
-ws1 = my_excel.active
-ws2 = my_excel.create_sheet("Sheet_2")
+class DataCharts(object):
+    def __init__(self, txt_file, excel_file):
+        self.txt_file = txt_file
+        self.excel_file_name = excel_file
 
-my_file = open('US_data.txt', 'r')
-column_letters = ["A", "B", "C", "D", "E", "F", "G"]
-row_number = 0
-column_number = 0
-for line in my_file:
-    elements = line[:-1].split("|")
-    if len(elements) > 1:
-        for element in elements:
-            try:
-                floatElement = float(element)
-                ws1[column_letters[column_number]+str(row_number + 1)] = floatElement
-            except:
-                ws1[column_letters[column_number]+str(row_number + 1)] = element
-            column_number += 1
-        row_number += 1
+    def txt_to_excel(self):
+        self.excel_file = Workbook()
+        ws1 = self.excel_file.active
+
+        file = open(self.txt_file, 'r')
+        column_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+        row_number = 0
         column_number = 0
+        for line in file:
+            elements = line.split("|")
+            if len(elements) > 1:
+                for element in elements:
+                    try:
+                        floatElement = float(element)
+                        ws1[column_letters[column_number]+str(row_number + 1)] = floatElement
+                    except:
+                        ws1[column_letters[column_number]+str(row_number + 1)] = element
+                    column_number += 1
+                row_number += 1
+                column_number = 0
+        self.excel_file.save(self.excel_file_name)
 
-data = Reference(ws1, min_col = 2, min_row = 1,
-                      max_col = 3, max_row = 15)
-titles = Reference(ws1, min_col = 1, min_row = 2, max_row = 15)
-US_chart = BarChart()
-US_chart.title = "Birth rate and Internet users in US - percentage"
-US_chart.add_data(data=data, titles_from_data=True)
-US_chart.set_categories(titles)
+    def create_chart(self, chart_title, anchor_cell, data_min_col, data_min_row, data_max_col, data_max_row, ref_min_col, ref_min_row, ref_max_row):
+        #excel_file_open = self.excel_file.load_workbook()
+        ws1 = self.excel_file.active
+        data = Reference(ws1, min_col = data_min_col, min_row = data_min_row, max_col = data_max_col, max_row = data_max_row)
+        titles = Reference(ws1, min_col = ref_min_col, min_row = ref_min_row, max_row = ref_max_row)
+        chart = BarChart()
+        chart.title = chart_title
+        chart.add_data(data=data, titles_from_data=True)
+        chart.set_categories(titles)
 
-ws1.add_chart(US_chart, "H3")
+        ws1.add_chart(chart, anchor_cell)
 
-my_excel.save("US_demographic_small.xlsx")
+        self.excel_file.save(self.excel_file_name)
+
+first_chart = DataCharts("US_data.txt", "US_data_class_sctipt.xlsx")
+first_chart.txt_to_excel()
+first_chart.create_chart("Birth and Internet", "H4", 2, 1, 3, 15, 1, 2, 15)
