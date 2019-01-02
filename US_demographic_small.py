8c23d6287e5e085b29b7930c369ee7446b9f097f
jamruzanka <m.w.jamrog@gmail.com>
2019-01-02 19:13:05 +0100

    cleaned up the script, added comments

--- US_demographic_small.py
+++ US_demographic_small.py
@@ -2,20 +2,23 @@ from openpyxl import Workbook
 from openpyxl.chart import Reference, Series, BarChart
 
 class DataCharts(object):
-    def __init__(self, txt_file, excel_file):
+    #to create an object, provide a txt's file name (existing one) and then the xlsx file (which will be created)
+    def __init__(self, txt_file, excel_file_name):
         self.txt_file = txt_file
-        self.excel_file_name = excel_file
+        self.excel_file_name = excel_file_name
 
-    def txt_to_excel(self):
+    def txt_to_excel(self, split_symbol):
+    # #to convert a txt file into a xlsx file, provide a split symbol used in the txt file - could be ",", "|" etc.
         self.excel_file = Workbook()
         ws1 = self.excel_file.active
 
         file = open(self.txt_file, 'r')
-        column_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+        #provided letters for 12 columns now - if there is more columns, please add further letters as they appear in excel
+        column_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
         row_number = 0
         column_number = 0
         for line in file:
-            elements = line.split("|")
+            elements = line.split(split_symbol)
             if len(elements) > 1:
                 for element in elements:
                     try:
@@ -29,7 +32,6 @@ class DataCharts(object):
         self.excel_file.save(self.excel_file_name)
 
     def create_chart(self, chart_title, anchor_cell, data_min_col, data_min_row, data_max_col, data_max_row, ref_min_col, ref_min_row, ref_max_row):
-        #excel_file_open = self.excel_file.load_workbook()
         ws1 = self.excel_file.active
         data = Reference(ws1, min_col = data_min_col, min_row = data_min_row, max_col = data_max_col, max_row = data_max_row)
         titles = Reference(ws1, min_col = ref_min_col, min_row = ref_min_row, max_row = ref_max_row)
@@ -42,6 +44,9 @@ class DataCharts(object):
 
         self.excel_file.save(self.excel_file_name)
 
-first_chart = DataCharts("US_data.txt", "US_data_class_sctipt.xlsx")
-first_chart.txt_to_excel()
+first_chart = DataCharts("US_data.txt", "US_data_class_script.xlsx")
+first_chart.txt_to_excel("|")
+#anchor_cell is the cell where the chart will be created - top left corner
+#the arguments after the anchor_cell indicate the data that will be used to create a chart:
+#data_min_col is the minimum column, data_min_row is the minimum row etc.). Without them, the script won't know which data should he visualize in the chart.
 first_chart.create_chart("Birth and Internet", "H4", 2, 1, 3, 15, 1, 2, 15)
